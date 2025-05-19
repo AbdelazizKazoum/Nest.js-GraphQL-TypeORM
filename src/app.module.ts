d19c6b559c01141c5d11f7dda1d46ca1936c63b2
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-05-19 12:44:08 +0100

    install  and run graphql with mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), '/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '051688azizKazoum@',
+      database: 'nest_GraphQL',
+      entities: [User],
+      synchronize: true,
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
